8d241f0a5f90fb864228a6692d46e3140b0c4ce9
OihanGonzalez <gonzalezoihan8@gmail.com>
2026-03-16 13:33:10 +0100

    Change CardController

--- src/main/java/com/dami/tcg/controller/CardController.java
+++ src/main/java/com/dami/tcg/controller/CardController.java
@@ -5,29 +5,29 @@ import java.util.List;
 import com.dami.tcg.modelo.*;
 
 public class CardController {
-	CardDAO dao = new ImplCardDAO();
+	CardDAO dao = new ImplCardBD();
 	
 	public boolean checkCard(Card card) {
-		return dao.checkCard(Card card);
+		return dao.checkCard(card);
 	}
 
     public boolean insertCard(Card card) {
-    	return dao.insertCard(Card card);
+    	return dao.insertCard(card);
     }
 
     public boolean deleteCard(Card card) {
-    	return dao.deleteCard(Card card);
+    	return dao.deleteCard(card);
     }
 
     public boolean updateCard(Card card) {
-    	return dao.updateCard(Card card);
+    	return dao.updateCard(card);
     }
 
     public List<Card> queryAll(){
     	return dao.queryAll();
     }
 
-    public Card queryCard(int cardId) {
-    	return dao.queryCard(int cardId);
+    public Card queryCard(String name) {
+    	return dao.queryCard(name);
     }
 }
