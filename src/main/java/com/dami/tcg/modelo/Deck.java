7b8072c092dfd97b19b2717f3fe377d35631236d
OihanGonzalez <gonzalezoihan8@gmail.com>
2026-03-16 12:55:51 +0100

    Create classes Deck and Card

--- src/main/java/com/dami/tcg/modelo/Deck.java
+++ src/main/java/com/dami/tcg/modelo/Deck.java
@@ -1,5 +1,54 @@
 package com.dami.tcg.modelo;
 
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+	private int deckID;
+	private String title;
+	private String description;
+	private ArrayList<Card> aCard;
+	
+	public Deck() {
+		this.deckID = 0;
+		this.title = "";
+		this.description = "";
+		this.aCard = new ArrayList<Card>();
+	}
+
+	public Deck(int deckID, String title, String description, ArrayList<Card> aCard) {
+		this.deckID = deckID;
+		this.title = title;
+		this.description = description;
+		this.aCard = aCard;
+	}
+
+	public int getDeckID() {
+		return deckID;
+	}
+	public void setDeckID(int deckID) {
+		this.deckID = deckID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public ArrayList<Card> getaCard() {
+		return aCard;
+	}
+	public void setaCard(ArrayList<Card> aCard) {
+		this.aCard = aCard;
+	}
+	
+	@Override
+	public String toString() {
+		return "Deck [deckID=" + deckID + ", title=" + title + ", description=" + description + ", aCard=" + aCard+ "]";
+	}
 }
