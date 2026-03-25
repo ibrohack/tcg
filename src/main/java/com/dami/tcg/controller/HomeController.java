5de3a62491f392c9c6a9b0787e480c7f56cb8ef3
ibrohack <bmromeroe@gmail.com>
2026-03-25 12:10:01 +0100

    Home page for the application, /resources/static/html/home.html for design reference. HomeController, StatsDAO and ImplStatsBD for backend, /templates/home.html for the front end

---
+++ src/main/java/com/dami/tcg/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.dami.tcg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.dami.tcg.modelo.ImplStatsBD;
+import com.dami.tcg.modelo.StatsDAO;
+import com.dami.tcg.modelo.Card;
+import java.util.List;
+
+@Controller
+public class HomeController {
+    StatsDAO statsDao = new ImplStatsBD();
+
+    @GetMapping("/")
+    public String home(Model model) {
+        int activePlayersCount = statsDao.getActivePlayersCount();
+        Card mostCommonCard = statsDao.getMostCommonCard();
+        int mostCommonQty = statsDao.getMostCommonCardQuantity();
+        Card leastFoundCard = statsDao.getLeastFoundCard();
+        int leastFoundQty = statsDao.getLeastFoundCardQuantity();
+        List<Card> unclaimedCards = statsDao.getUnclaimedCards();
+        List<Card> latestCards = statsDao.getLatestCards(4);
+
+        model.addAttribute("activePlayersCount", activePlayersCount);
+        model.addAttribute("mostCommonCard", mostCommonCard);
+        model.addAttribute("mostCommonQty", mostCommonQty);
+        model.addAttribute("leastFoundCard", leastFoundCard);
+        model.addAttribute("leastFoundQty", leastFoundQty);
+        
+        if (unclaimedCards != null && !unclaimedCards.isEmpty()) {
+            model.addAttribute("unclaimedCard", unclaimedCards.get(0));
+            model.addAttribute("hasUnclaimed", true);
+        } else {
+            model.addAttribute("hasUnclaimed", false);
+        }
+        
+        model.addAttribute("latestCards", latestCards);
+
+        return "home";
+    }
+}
