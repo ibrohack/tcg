b5095392313f3ac28a153364b2081d14f5eb94fe
AdamFixed <adamnr012@gmail.com>
2026-04-15 11:51:03 +0200

    Made Deck View better on deck check and dashboard

--- src/main/java/com/dami/tcg/controller/DeckController.java
+++ src/main/java/com/dami/tcg/controller/DeckController.java
@@ -115,4 +115,28 @@ public class DeckController {
 		}
 	}
 
+	@GetMapping("/deckedit")
+	public String deckEdit(Model model, HttpSession session, RedirectAttributes redirectAttributes,
+			@RequestParam int deckId) {
+		Player player = (Player) session.getAttribute("loggedPlayer");
+		if (player == null) {
+			return "redirect:/login";
+		}
+		Deck deck = dao.queryDeck(deckId);
+		if (deck.getCards().isEmpty()) {
+			redirectAttributes.addFlashAttribute("error", "Deck not found or no cards");
+			return "redirect:/deckcheck";
+		} else {
+			ArrayList<Card> cards = new ArrayList<Card>();
+			for (int cardId : deck.getCards().keySet()) {
+				cards.add(cardDao.queryCardId(cardId));
+			}
+			model.addAttribute("cardQuantities", deck.getCards());
+			model.addAttribute("deck", deck);
+			model.addAttribute("player", player);
+			model.addAttribute("cards", cards);
+			return "deckedit";
+		}
+	}
+
 }
