6c86a15a0cf9cf7ec13a227413d1f6bdd7203afb
SrEstroncio038 <asolagurenanaut@gmail.com>
2026-04-16 09:05:43 +0200

    generated new JavaDoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.dami.tcg.controller"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.dami.tcg.config"}];updateSearchResults();
